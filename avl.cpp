0597507253bb55b155b22b9ade92e2b1486bf67f
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-04-18 14:58:31 +0530

    AVL tree

--- avl.cpp
+++ avl.cpp
@@ -1,7 +1,28 @@
-#include<iostream> 
+#include<iostream>
 #include<stdlib.h>
 
-// PERFECTECTLY WORKING CODE FOR INSERTION IN AVL TREE(PERFORMS ROTATIONS AS WELL)
+// PERFECTLY WORKING CODE FOR AVL TREE!!!!!
+// DELETION, INSERTION PERFORMED NICELY: ROTATIONS PERFORMED NICELY!!!!
+
+/* REVIEW THE DELETION CODE FOR THE AVL TREE, AND RUN THE CODE!!! THE BALANCE FACTORS OF THE DELETED NODES HAVE NOT YET BEEN
+ * CALCULATED!!!!
+*/
+
+/* READ THIS BEFORE PROCEEDING FURTHER FOR DELETION
+ * after deletion of a node just call blntree(node*); which will traverse the whole tree and in the unwinding phase, it will perform
+ * functions similar to insert() function (like calling bf() and checkBalance())
+*/
+
+/* DELETION IN AVL TREE HAS TO BE DONE SOMEWHAT LIKE DELETION IN BST(ALONG WITH ROTATIONS)
+   THE FOLLOWING CASES OF BST WILL BE USED:
+   -> NODE TO BE DELETED HAS ONE CHILD(LEFT OR RIGHT)
+   -> NODE TO BE DELETED HAS TWO CHILDREN : SEARCH FOR INORDER SUCCESSOR
+
+   ROTATIONS WILL BE DONE ACCORDING TO THE VALUES OF BALANCE FACTORS OF THE NODES
+*/
+
+// PERFECTLY WORKING CODE FOR INSERTION IN AVL TREE(PERFORMS ROTATIONS AS WELL)
+
 /*
    INSERTION DONE IN LEFT SUBTREE:
    -> NODE WAS INSERTED IN THE LEFT SUBTREE OF THIS SUBTREE - perform right rotation about node p
@@ -9,7 +30,6 @@
 
    INSERTION DONE IN RIGHT SUBTREE:
    -> NODE WAS INSERTED IN THE RIGHT SUBTREE OF THIS SUBTREE - perform left rotation about p
-   -> NODE WAS INSERTED IN THE LEFT SUBTREE OF THIS SUBTREE - perform right rotation
    -> NODE WAS INSERTED IN THE LEFT SUBTREE OF THIS SUBTREE - perform rightleft rotation
 */
 
@@ -25,33 +45,40 @@ struct node
 
 node*root=NULL, *parent=NULL, *a=NULL;
 node*insert(node*, int);
-node*searchPar(node*, int);
+node*searchPar(node*, node*); // search the parent of the current node(if any)
+node*searchInorder(node*); // search the inorder successor of the node to be deleted
+node*searchNode(node*, int); // search a node
+int checkChildren(node*);
+void checkDelBln(node**); // check the balance factor of a node after deletion of a node from that subtree!!!
 void preorder(node*);
 void inorder(node*);
 void postorder(node*);
+int blntree(node*); // to calculate balance factors after deletion of nodes from the tree!!!
 int bf(node*); // balance factor
 int height(node*); // height of the tree
 void checkBalance(node**);
 void rotateRight(node*, node**);
 void rotateLeft(node*, node**);
-/*void deleteNode(parameters go here); // to delete nodes*/
+node*delNode(node*, node*); // to delete nodes, takes an int argument to see if the node to be deleted has one or two or no children!!!
 void terminate(node*);
 
 
 int main(void)
 {
 	int opt, key;
+	node*foundNode=NULL;
 
 	while(1)
 	{
 		cout<<"\n\n---menu---\n\n";
 		cout<<"1. Insert.\n";
 		cout<<"2. Calculate height.\n";
-		cout<<"3. Preorder.\n";
-		cout<<"4. Inorder.\n";
-		cout<<"5. Postorder.\n";
-		cout<<"6. Delete.\n";
-		cout<<"7. Exit.\n";
+		cout<<"3. Search.\n";
+		cout<<"4. Preorder.\n";
+		cout<<"5. Inorder.\n";
+		cout<<"6. Postorder.\n";
+		cout<<"7. Delete.\n";
+		cout<<"8. Exit.\n";
 
 		cout<<"[OPTION]";
 		cin>>opt;
@@ -67,18 +94,39 @@ int main(void)
 				cout<<"\nheight of the tree is: "<<height(root);
 				break;
 			case 3:
-				preorder(root);
+				cout<<"enter node to be searched: ";
+				cin>>key;
+				foundNode=searchNode(root, key);
+				if(foundNode==NULL)
+					cout<<foundNode->key<<" is in the tree!\n";
+				else
+					cout<<key<<" is not in the tree!\n";
 				break;
 			case 4:
-				inorder(root);
+				preorder(root);
 				break;
 			case 5:
-				postorder(root);
+				inorder(root);
 				break;
 			case 6:
-				/*left for later*/
+				postorder(root);
 				break;
 			case 7:
+				cout<<"enter node to be deleted: ";
+				cin>>key;
+                                foundNode=searchNode(root, key);
+				if(foundNode==NULL)
+                                {
+					cout<<key<<" is not in the tree!!\n\n";
+                                }
+                                
+                                else
+                                {
+                                        parent=searchPar(root, foundNode);
+                                       root=delNode(parent, foundNode); // delete foundNode
+                                }
+				break;
+			case 8:
 				if(root==NULL)
 					exit(0);
 				else
@@ -120,6 +168,186 @@ node*insert(node*r, int key)
 	return r;
 }
 
+node*searchNode(node*r, int key)
+{
+	while(r!=NULL)
+	{
+		if(r->key==key)
+			break;
+		else if(key<r->key)
+			r=r->left;
+		else if(key>r->key)
+			r=r->right;
+	}
+
+
+	return r;
+
+}
+
+int checkChildren(node*r)
+{
+	if(r->left==NULL && r->right==NULL)
+		return 0;
+	else if(r->left==NULL && r->right!=NULL)
+		return 1;
+	else if(r->left!=NULL && r->right==NULL)
+		return 1;
+	else
+		return 2;
+}
+
+
+node*searchInorder(node*n)
+{
+        n=n->right;
+        
+        while(n->left!=NULL)
+                n=n->left;
+        
+        return n;
+}
+
+node* delNode(node*parent, node*child) // child will be deleted!!
+{
+        node*inorderS=NULL;
+        if(checkChildren(child)==2) // if child has two children
+        {
+                inorderS=searchInorder(child);
+                parent=searchPar(root, inorderS);
+                /* since parent has no use with child so inorderS's parent will be searched*/
+                child->key=inorderS->key; // copying the value of inorder successor into child
+                
+                if(checkChildren(inorderS)==1)
+                        parent->left==inorderS?parent->left=inorderS->right:parent->right=inorderS->right;
+                
+                else
+                        parent->left==inorderS?parent->left=NULL:parent->right=NULL;
+                
+                delete inorderS; // inorder successor is deleted!!!
+                blntree(root); // call checkBalance() in this function
+        }
+        
+        else if(checkChildren(child)==1) // if child has one child
+        {
+                if(parent->left==child)
+                {
+                        if(child->right!=NULL)
+                        {
+                                parent->left=child->right;
+                        }
+                        
+                        else
+                        {
+                                parent->left=child->left;
+                        }
+                }
+                
+                else
+                {
+                        if(child->right!=NULL)
+                        {
+                                parent->right=child->right;
+                        }
+                        
+                        else
+                        {
+                                parent->right=child->left;
+                        }
+                }
+                
+                delete child;
+                blntree(root);
+        }
+        
+        else // if child has no children
+        {
+                if(parent->left==child)
+                        parent->left=NULL;
+                else
+                        parent->right=NULL;
+                
+                delete child;
+                blntree(root);
+        }
+        
+        return root;
+}
+
+
+int blntree(node*r)
+{
+        int hL, hR;
+        if(r==NULL)
+                return 0;
+        hL=1+blntree(r->left);
+        hR=1+blntree(r->right);
+        
+        r->balance=hL-hR;
+        checkDelBln(&r);
+        
+        if(hL>hR)
+                return hL;
+        else
+                return hR;
+}
+
+void checkDelBln(node**r)
+{
+        switch((*r)->balance)
+        {
+                case 2: // deletion was done from the right subtree
+                        // similar to insertion in the left subtree
+                        switch((*r)->left->balance)
+                        {
+                                case 0:
+                                        // right rotation about node r
+                                        parent=searchPar(root, (*r));
+                                        rotateRight(parent, r);
+                                        break;
+                                case 1:
+                                        // right rotation about node r
+                                        parent=searchPar(root, (*r));
+                                        rotateRight(parent, r);
+                                        break;
+                                case -1: // special case
+                                        // leftright rotation
+                                        a=(*r)->left;
+                                        rotateLeft(*r, &a);
+                                        parent=searchPar(root, (*r));
+                                        rotateRight(parent, r);
+                                        break;
+                        }
+                        
+                        break;
+                        
+                case -2: // deletion was done from the left subtree
+                         // similar to insertion in the right subtree
+                        switch((*r)->right->balance)
+                        {
+                                case 0:
+                                        // left rotation about node r
+                                        parent=searchPar(root, (*r));
+                                        rotateLeft(parent, r);
+                                        break;
+                                case -1:
+                                        // left rotation about node r
+                                        parent=searchPar(root, (*r));
+                                        rotateLeft(parent, r);
+                                        break;
+                                case 1: // special case
+                                        // rightleft rotation
+                                        a=(*r)->right;
+                                        rotateRight(*r, &a);
+                                        parent=searchPar(root, (*r));
+                                        rotateLeft(parent, r);
+                                        break;
+                        }
+                        
+                        break;
+        }
+}
+
 void checkBalance(node**r)
 {
 	switch((*r)->balance)
@@ -131,7 +359,7 @@ void checkBalance(node**r)
 				case 1:
 					// insertion was done in the left subtree of this subtree
 					/*call a function to balance the node p*/
-					parent=searchPar(root, (*r)->key);
+					parent=searchPar(root, (*r));
 					rotateRight(parent, r);
 					break;
 
@@ -140,7 +368,7 @@ void checkBalance(node**r)
 					/*call a function to balance the unbalanced node*/
 					a=(*r)->left;
 					rotateLeft(*r, &a);
-					parent=searchPar(root, (*r)->key);
+					parent=searchPar(root, (*r));
 					rotateRight(parent, r);
 					break;
 			}
@@ -154,13 +382,13 @@ void checkBalance(node**r)
 					// insertion was done in the left subtree of this subtree
 					a=(*r)->right;
 					rotateRight(*r, &a);
-					parent=searchPar(root, (*r)->key);
+					parent=searchPar(root, (*r));
 					rotateLeft(parent, r);
 					break;
 
 				case -1:
 					// insertion was done in the right subtree of this subtree
-					parent=searchPar(root, (*r)->key);
+					parent=searchPar(root, (*r));
 					rotateLeft(parent, r);
 					break;
 			}
@@ -171,17 +399,17 @@ void checkBalance(node**r)
 	return;
 }
 
-node*searchPar(node*r, int key)
+node*searchPar(node*r, node*n)
 {
-	if(r->key==key)
+	if(r->key==n->key)
 		return r;
 	while(r!=NULL)
 	{
-		if(r->left->key==key || r->right->key==key)
+		if(r->left->key==n->key || r->right->key==n->key)
 			return r;
-		else if(key<r->key)
+		else if(n->key<r->key)
 			r=r->left;
-		else if(key>r->key)
+		else if(n->key>r->key)
 			r=r->right;
 	}
 
